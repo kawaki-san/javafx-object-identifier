4ad05dca206e4eff2c2f2993c55a481348bba0a0
Kawaki-san <rtkayy123@gmail.com>
2021-02-05 15:25:01 +0200

    adds jpeg extension

--- app/src/main/java/com/rtkay/Controller.java
+++ app/src/main/java/com/rtkay/Controller.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -70,7 +71,8 @@ public class Controller implements Initializable {
         Stage stage = (Stage) imgPreview.getScene().getWindow();
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
-        fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+        fileChooser.getExtensionFilters()
+                .addAll(new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif", "*.jpeg"));
         File selectedFile = fileChooser.showOpenDialog(stage);
         selectedImage = selectedFile;
         if (selectedFile != null) {
@@ -89,23 +91,59 @@ public class Controller implements Initializable {
         try {
             imageBytes = Files.readAllBytes(selectedImage.toPath());
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        try (Tensor image = Tensor.create(imageBytes)) {
+            float[] labelProbabilities = executeInceptionGraph(graphDef, image);
+            int bestLabelIndex = maxIndex(labelProbabilities);
+            txtResults.setText("");
+            txtResults.setText(String.format("BEST MATCH: %s (%.2f%% likely)", labels.get(bestLabelIndex),
+                    labelProbabilities[bestLabelIndex] * 100f));
+            System.out.println(String.format("BEST MATCH: %s (%.2f%% likely)", labels.get(bestLabelIndex),
+                    labelProbabilities[bestLabelIndex] * 100f));
+        }
+    }
 
+    private int maxIndex(float[] probabilities) {
+        int best = 0;
+        for (int i = 1; i < probabilities.length; ++i) {
+            if (probabilities[i] > probabilities[best]) {
+                best = i;
+            }
+        }
+        return best;
+    }
+
+    private float[] executeInceptionGraph(byte[] graphDef2, Tensor image) {
+        try (Graph g = new Graph()) {
+            g.importGraphDef(graphDef);
+            try (Session s = new Session(g);
+                    Tensor result = s.runner().feed("DecodeJpeg/contents", image).fetch("softmax").run().get(0)) {
+                final long[] rshape = result.shape();
+                if (result.numDimensions() != 2 || rshape[0] != 1) {
+                    throw new RuntimeException(String.format(
+                            "Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
+                            Arrays.toString(rshape)));
+                }
+                int nlabels = (int) rshape[1];
+                float[][] here = new float[1][nlabels];
+                float[] probabilities = ((float[][]) result.copyTo(here))[0];
+                return probabilities;
+            }
+        }
     }
 
     /* stores a model of our known data into an array list */
     private List<String> copyLinesToList(String path) throws IOException {
         URL url = getClass().getResource(path);
         File file = new File(url.getFile());
-        Scanner s = new Scanner(file);
-        ArrayList<String> list = new ArrayList<String>();
-        while (s.hasNextLine()) {
-            list.add(s.next());
+        try {
+            return Files.readAllLines(file.toPath(), Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+            System.exit(0);
         }
-        s.close();
-        return list;
+        return null;
     }
 
 }
